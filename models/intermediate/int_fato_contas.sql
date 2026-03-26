cd87fe471dc005e15ff5801103bea0a72b4993cc
JEYSEL PACHECO BASTOS <jeysel@gmail.com>
2026-03-26 21:30:31 +0000

    Concluido contrução da feature fato transações. DBT BUILD executou com sucesso, sem erros

---
+++ models/intermediate/int_fato_contas.sql
@@ -0,0 +1,19 @@
+with
+    contas as (
+        select *
+        from {{ ref('stg_erp__contas')}}
+    )
+
+    , selecionar_colunas as (
+        select 
+            pk_conta
+            , fk_cliente 
+            , fk_agencia 
+            , saldo_total
+            , saldo_disponivel
+            , ts_ultimo_lancamento
+        from contas
+    )
+
+select *
+from contas
